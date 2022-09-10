f83414a42effeb3be5b938457e293cbbcd7eeb73
Bear_bear <unegbuclinton5@gmail.com>
2022-09-10 15:17:46 +0100

    update project

---
+++ frontEnd/src/icons/indxe.jsx
@@ -0,0 +1,16 @@
+export { ReactComponent as DPIconAdd } from './svgs/addButton.svg';
+export { ReactComponent as DPIconBars } from './svgs/bars.svg';
+export { ReactComponent as DPIconCancel } from './svgs/cancel.svg';
+export { ReactComponent as DPIconCheck } from './svgs/checked.svg';
+export { ReactComponent as DPIconCompleted } from './svgs/complete.svg';
+export { ReactComponent as DPIconHide } from './svgs/hide.svg';
+export { ReactComponent as DPIconHistory } from './svgs/history.svg';
+export { ReactComponent as DPIconHome } from './svgs/home.svg';
+export { ReactComponent as DPIconLoginLogo } from './svgs/login-logo.svg';
+export { ReactComponent as DPIconLogout } from './svgs/logout.svg';
+export { ReactComponent as DPIconRegisterLogo } from './svgs/register-logo.svg';
+export { ReactComponent as DPIconSpinner } from './svgs/spinne.svg';
+export { ReactComponent as DPIconStats } from './svgs/stats.svg';
+export { ReactComponent as DPIconTaskIcon } from './svgs/task.svg';
+export { ReactComponent as DPIconTodo } from './svgs/todo.svg';
+export { ReactComponent as DPIconUnhide } from './svgs/unhide.svg';
--- signupBackend/server.js
+++ signupBackend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const routes = require('./routes/route');
+const path = require('path');
 const cors = require('cors');
 
 dotenv.config();
@@ -14,5 +15,18 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 routes(app);
+
+app.use(express.static(path.join(__dirname, '../frontEnd/build')));
+
+app.get('*', function (_, res) {
+  res.sendFile(
+    path.join(__dirname, '../frontEnd/build/index.html'),
+    function (err) {
+      if (err) {
+        res.status(500).send(err);
+      }
+    }
+  );
+});
 
 app.listen(4000, () => console.log('Server is listening......'));
